7197420ef55ff2e5f001dfc9b121b605ec5b526c
Silva <GabrielSilva@gmail.com>
2021-12-01 12:46:31 -0300

    Fazendo o atrib2

--- ContagemRef.c
+++ ContagemRef.c
@@ -9,25 +9,65 @@ hp *heap;
 
 void IniciarHeap(){
     heap=(hp *) malloc(sizeof(hp));
-    heap->ct=0;
 }
-int add=0;
+int pos=0;
 
 int *malloc2(int tam){
     heap= realloc(heap, sizeof(hp)*1);
-    heap[add].ct++;
-    printf("\n%d\n",&heap[0].val);
-    return &heap[0].val;
+    heap[pos].ct=1;
+    printf("\n%d\n",&heap[pos].val);
+    pos++;
+    return &heap[pos-1].val;
+}
+
+void atrib2(int *a, int *b){
+    int enda= *a;
+    int endhp= heap;
+    int t=sizeof(hp);
+
+    int p=(endhp - enda)/t;
+
+    heap[p].ct--;
+    *a=*b;
 }
 
 int main(){
     //Iniciando heap
     IniciarHeap();
 
+    //malloc2
+    //printf("Maloc do V: \n");
     int *v=malloc2(sizeof(int));
     *v=13;
+    /*
     printf("\n%d\n",v);
     printf("\n%d\n",*v);
     printf("\n%d\n",heap[0].val);
+    printf("\n%d\n\n",heap[0].ct);*/
+
+    *v=15;
+    /*
+    printf("\nV=15\n");
+    printf("\n%d\n",v);
+    printf("\n%d\n",*v);
+    printf("\n%d\n",heap[0].val);
+    printf("\n%d\n\n",heap[0].ct);
+
+
+    printf("Maloc do W: \n");*/
+    int *w = malloc2(sizeof(int));
+    *w=26;
+
+    /*printf("\n%d\n",w);
+    printf("\n%d\n",*w);
+    printf("\n%d\n",heap[1].val);
+    printf("\n%d\n\n",heap[1].ct);*/
+
+
+    atrib2(&v,&w);
+    printf("\n%d\n\n",heap[0].ct);
+    /*printf("&V=%d &W=%d ",v,w);
+
+    printf("V=%d W=%d ",*v,*w);*/
     
 }

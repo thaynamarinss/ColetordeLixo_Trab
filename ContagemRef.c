f4e5494a8662e0e95a8b4bfc9c6c68136671d7ed
Silva <GabrielSilva@gmail.com>
2021-12-01 14:39:11 -0300

    Adicionando Coleta de Lixo, e tentando fazer funcionar

--- ContagemRef.c
+++ ContagemRef.c
@@ -19,8 +19,16 @@ int *malloc2(int tam){
     pos++;
     return &heap[pos-1].val;
 }
+void PrintaHeap(){
+    for(int i=0;i<pos;i++){
+        printf("val=%d ct=%d\n",heap[i].val,heap[i].ct);
+    }printf("\n");
+}
 
 void atrib2(int *a, int *b){
+    for(int i=0;i<pos;i++){
+        printf("%d val=%d\n", &heap[i], heap[i].val);
+    }
     int enda= *a;
     int endhp= heap;
     int t=sizeof(hp);
@@ -29,7 +37,27 @@ void atrib2(int *a, int *b){
 
     heap[p].ct--;
     *a=*b;
+    for(int i=0;i<pos;i++){
+        printf("%d val=%d\n", &heap[i], heap[i].val);
+    }
+    if(heap[p].ct==0) ColetaLixo(p);
+
+    for(int i=0;i<pos;i++){
+        printf("%d val=%d\n", &heap[i], heap[i].val);
+    }
 }
+// | w | c | 
+void ColetaLixo(int p){
+    
+    for(int i=p;i<pos-1;i++){
+        heap[i]=heap[i+1];
+    }
+    free(&heap[pos]);
+    pos--;
+    heap = realloc(heap,sizeof(hp)*pos-1);
+    
+}
+
 
 int main(){
     //Iniciando heap
@@ -57,17 +85,22 @@ int main(){
     printf("Maloc do W: \n");*/
     int *w = malloc2(sizeof(int));
     *w=26;
-
+    
+    int *c = malloc2(sizeof(int));
+    *c=17;
     /*printf("\n%d\n",w);
     printf("\n%d\n",*w);
     printf("\n%d\n",heap[1].val);
     printf("\n%d\n\n",heap[1].ct);*/
 
-
+    printf("\n&V=%d &W=%d\n",v,w);
+    printf("\n&V=%d &W=%d\n",*v,*w);
+    PrintaHeap();
     atrib2(&v,&w);
-    printf("\n%d\n\n",heap[0].ct);
-    /*printf("&V=%d &W=%d ",v,w);
+    PrintaHeap();
+    printf("\n&V=%d &W=%d\n",v,w);
+    printf("\n&V=%d &W=%d\n",*v,*w);
 
-    printf("V=%d W=%d ",*v,*w);*/
-    
+    int *d = malloc2(sizeof(int));
+    *d=19;
 }

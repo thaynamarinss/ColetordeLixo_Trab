12a8ba89dd754f821783ca5c544fed24e8eefc68
Silva <GabrielSilva@gmail.com>
2021-12-01 19:41:02 -0300

    Epifania Computacional feita e concretizada

--- ContagemRef.c
+++ ContagemRef.c
@@ -1,104 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 typedef struct hp{
-    int val;
+    int valor;
     int ct;
 }hp;
 hp *heap;
-
-void IniciarHeap(){
-    heap=(hp *) malloc(sizeof(hp));
-}
 int pos=0;
 
 int *malloc2(int tam){
-    heap= realloc(heap, sizeof(hp)*1);
+    heap=(hp *) realloc(heap,sizeof(hp));
     heap[pos].ct=1;
-    printf("Print - Func Malloc2 heap[%d]%d\n",pos, &heap[pos].val);
     pos++;
-    return &heap[pos-1].val;
+    return &heap[pos-1];
 }
+
+void atrib2(int *apontador, int *receptor){
+    //Pegando endere√ßo do apontador e o tamanho em bytes de cada "casa" do vetor
+    int AdressApontador=*apontador;
+    int AdressReceptor=*receptor;
+    int AdressHeap= heap;
+    int tamanho=sizeof(hp);
+    int pA=(AdressApontador - AdressHeap) / tamanho;
+    int pR=(AdressReceptor - AdressHeap) / tamanho;
+    
+    heap[pA].ct--;
+
+    int count=heap[pR].ct + 1;
+    int val=heap[pR].valor;
+
+    if(heap[pA].ct==0){
+        ColetordeLixo(pA);
+        heap[pR-1].ct=count;
+        heap[pR-1].valor=val;
+        *receptor-=tamanho;
+    }
+    *apontador=*receptor;
+}
+
+void ColetordeLixo(int p){
+    for(int i=p;i<pos-1;i++)
+        heap[i]=heap[i-1];
+    pos--;
+    heap=(hp*) realloc(heap,sizeof(hp)*pos);
+}
+
 void PrintaHeap(){
     for(int i=0;i<pos;i++){
-        printf("Heap[%d], Adress: %d val=%d ct=%d\n",i,&heap[i], heap[i].val,heap[i].ct);
-    }printf("\n");
-}
-
-// | w | c |
-void ColetaLixo(int p){
-
-    for(int i=p;i<pos-1;i++){
-        heap[i]=heap[i+1];
-    }
-    free(&heap[pos]);
-    pos--;
-    heap = realloc(heap,sizeof(hp)*pos-1);
-
-}
-
-
-void atrib2(int *a, int *b){
-    for(int i=0;i<pos;i++){
-        printf("(Print 1 - Func atrib2) Heap[%d] Adress: %d val=%d\n",i, &heap[i], heap[i].val);
-    }
-    int AdressA = a;
-    int AdressHeap = heap;
-    int t = sizeof(hp);
-
-    int p = (AdressHeap - AdressA)/t;
-
-    heap[p].ct--;
-    *a=*b;
-
-    for(int i=0;i<pos;i++){
-        printf("(Print 2 - Func atrib2 antes da coleta) EndereÁo de Heap[%d]: %d val=%d\n",i, &heap[i], heap[i].val);
-    }
-    if(heap[p].ct==0) ColetaLixo(p);
-
-    for(int i=0;i<pos;i++){
-        printf("(Print 3 - Func atrib2 pÛs coleta) EndereÁo de Heap[%d]: %d val=%d\n",i, &heap[i], heap[i].val);
+        printf("Heap[%d] End:%d ct:%d Valor:%d\n",i,&heap[i],heap[i].ct,heap[i].valor);
     }
 }
+
 
 int main(){
-	setlocale(LC_CTYPE, "Portuguese");
-
-    //Iniciando heap
-    IniciarHeap();
-
-    //malloc2
-    //printf("Maloc do V: \n");
     int *v=malloc2(sizeof(int));
     *v=10;
-    /*
-    printf("\nV=15\n");
-    printf("\n%d\n",v);
-    printf("\n%d\n",*v);
-    printf("\n%d\n",heap[0].val);
-    printf("\n%d\n\n",heap[0].ct);
-
-
-    printf("Maloc do W: \n");*/
-    int *w = malloc2(sizeof(int));
-    *w=15;
-
-    int *c = malloc2(sizeof(int));
-    *c=20;
-    /*printf("\n%d\n",w);
-    printf("\n%d\n",*w);
-    printf("\n%d\n",heap[1].val);
-    printf("\n%d\n\n",heap[1].ct);*/
-
-    printf("\n&V=%d &W=%d\n",v,w);
-    printf("\n&V=%d &W=%d\n",*v,*w);
     PrintaHeap();
-    atrib2(&v,&w);
+    int *w=malloc2(sizeof(int));
+    *w =20;
     PrintaHeap();
-    printf("\n&V=%d &W=%d\n",v,w);
-    printf("\n&V=%d &W=%d\n",*v,*w);
-
-    int *d = malloc2(sizeof(int));
-    *d=25;
+    printf("\n%d %d %d\n",&v,*v,v);
+    printf("\n%d %d %d\n",&w,*w,w);
+    atrib2(&v, &w);
+    PrintaHeap();
+    printf("\n%d %d %d\n",&v,*v,v);
+    printf("\n%d %d %d\n",&w,*w,w);
+    int *c=malloc2(sizeof(int));
+    *c=26;
+    printf("\n%d %d %d\n",&v,*v,v);
+    printf("\n%d %d %d\n",&w,*w,w);
+    printf("\n%d %d %d\n",&c,*c,c);
+    atrib2(&v, &c);
+    printf("\n%d %d %d\n",&v,*v,v);
+    printf("\n%d %d %d\n",&w,*w,w);
+    printf("\n%d %d %d\n",&c,*c,c);
+    PrintaHeap();
+    return 0;
 }
